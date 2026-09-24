3c11f66ba79bd2f60faa226aefca244dbd26486e
Maria Korlyakova <mariakorlyakowa@gmail.com>
2026-09-24 16:33:10 +0300

    Add table pagination

--- 1sttry/find_genotype/forms.py
+++ 1sttry/find_genotype/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from .models import Sample
 
 class GenotypeRequestForm(forms.Form):
-    chrom = forms.CharField(label="Input chromosome number, example: chr1", max_length=50)
+    chrom = forms.CharField(label="Input chromosome number (example chr1)", max_length=50)
     start = forms.IntegerField(label="Input start position", min_value=1)
     end = forms.IntegerField(label="Input end position", min_value=1)
     sample = forms.ModelChoiceField(queryset=Sample.objects.all(), empty_label="(All samples)", required=False)
--- 1sttry/find_genotype/views.py
+++ 1sttry/find_genotype/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Genotype
 from .forms import GenotypeRequestForm
 
 def find_genotype(request):
     form = GenotypeRequestForm(request.GET or None)
-    genotypes = None
+    page = None
     if form.is_valid():
         f = form.cleaned_data
         genotypes = Genotype.objects.select_related("coordinate__chromosome", "sample").filter(coordinate__pos__range=(f["start"], f["end"]), coordinate__chromosome__chrom=f["chrom"])
         if f["sample"] is not None:
             genotypes = genotypes.filter(sample=f["sample"])
-    return render(request, "find_genotype/find_genotype.html", {"genotypes": genotypes, "form": form})
+        paginator = Paginator(genotypes, 50)
+        page = paginator.get_page(request.GET.get("page"))
+    return render(request, "find_genotype/find_genotype.html", {"page": page, "form": form})
     
 
